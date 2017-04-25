5ae80f86c9f2c55ecd699a30d1029f007ba0a236
Peter Halloran <mkwenhua101@gmail.com>
2017-04-24 20:58:00 -0500

    refactor: remove commented out code

--- public/js/store.js
+++ public/js/store.js
@@ -1,15 +1,7 @@
-import { compose, createStore, applyMiddleware, combineReducers } from 'redux';
+import {compose, createStore, applyMiddleware, combineReducers} from 'redux';
 import logger from "redux-logger"
-//import thunk from "redux-thunk"
-//import promise from "redux-promise-middleware"
 
 import reducer from "./reducers"
+const composedStore = compose(applyMiddleware(logger()),);
 
-//const middleware = applyMiddleware(promise(), thunk, logger())
-//const middleware = applyMiddleware(promise())
- const composedStore = compose(
-    applyMiddleware(logger()),
-  );
-
- export default  composedStore(createStore)(reducer)
-//export default createStore(reducer);
+export default composedStore(createStore)(reducer)
